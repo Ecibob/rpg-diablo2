4548b04111a025b63804e1410a22d94e05778d96
Ecibob <keithariebecker@gmail.com>
2022-02-28 21:19:29 -0600

    First Commit of D2
    First Commit

---
+++ data.js
@@ -0,0 +1,32 @@
+const characterData = {
+    hero: {
+        name: "Amazon",
+        avatar: "images/amazon.gif",
+        health: 50,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    sorc: {
+        name: "Sorceress",
+        avatar: "images/sorceress.gif",
+        health: 30,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    necro: {
+        name: "Necro",
+        avatar: "images/necro.gif",
+        health: 25,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    pally: {
+        name: "Paladin",
+        avatar: "images/pally.gif",
+        health: 20,
+        diceCount: 3,
+        currentDiceScore: []
+    }
+}
+
+export default characterData
